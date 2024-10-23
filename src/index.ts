f8e4457ac60673e1aa22d09013321e8c3195baa6
Mashood ur Rehman Rana <ranamashood@proton.me>
2024-10-23 11:40:11 +0500

    feat(api): get all songs

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from "express";
+import { Server } from "socket.io";
+import http from "http";
+import fs from "fs";
+import cors from "cors";
+
+const PORT = 5000;
+
+const app: Application = express();
+const server: http.Server = http.createServer(app);
+const io: Server = new Server(server);
+
+app.use(cors());
+
+app.get("/get-all-songs", (req: Request, res: Response) => {
+  fs.readdir("./public/uploads/", (err, songs) => res.json({ songs }));
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connected.");
+});
+
+server.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`),
+);

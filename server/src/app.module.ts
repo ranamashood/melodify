a3f4ce4f917ffe84b8262125d362a2217d9e5e0c
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-05-13 00:52:37 +0500

    feat: add static file serving for images

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/melodifyDB'),
+    ServeStaticModule.forRoot({
+      rootPath: 'public/uploads/images',
+      serveRoot: '/images',
+    }),
     SongsModule,
   ],
   controllers: [AppController],

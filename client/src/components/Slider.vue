33db947fccef945ee7778c2abbfa8b4ceca29502
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-05-14 17:19:12 +0500

    feat: add `Slider` component
    it will be used for multiple things, such as:
    - volume slider
    - seek slider

---
+++ client/src/components/Slider.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  max: number
+  value: number
+  width: number
+}>()
+</script>
+
+<template>
+  <input
+    class="slider"
+    type="range"
+    min="0"
+    :max="max"
+    :value="value"
+    :style="{ width: width + 'px' }"
+    @change="$emit('change', $event)"
+  />
+</template>
+
+<style scoped>
+.slider {
+  cursor: pointer;
+}
+</style>

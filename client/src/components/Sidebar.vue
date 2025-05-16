735dd09d8b3eef094a0114ee81e17e0ff9f5f144
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-05-16 16:43:37 +0500

    feat: add `Sidebar`

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { fetchSongs } from '@/helpers'
+</script>
+
+<template>
+  <div class="sidebar">
+    <button class="sidebar__button" @click="fetchSongs('songs')">All Songs</button>
+    <button class="sidebar__button">Liked Songs</button>
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  position: sticky;
+  top: 0;
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 20px 10px;
+}
+
+.sidebar__button {
+  width: 100%;
+  border: none;
+  outline: none;
+  padding: 10px 0;
+  border-radius: 10px;
+  cursor: pointer;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue'
 import SeekBar from '@/components/SongPlayer.vue'
 import SongsList from '@/components/SongsList.vue'
 </script>
 
 <template>
-  <main>
-    <SongsList />
+  <div class="layout">
+    <aside>
+      <Sidebar />
+    </aside>
+    <main>
+      <SongsList />
+    </main>
     <SeekBar />
-  </main>
+  </div>
 </template>
+
+<style scoped>
+.layout {
+  display: flex;
+}
+main {
+  flex: 1;
+}
+</style>

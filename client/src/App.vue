8944fd67ea690f82de675d302b78ac56087c5177
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-06-24 23:13:04 +0500

    fix: login page keeps on reloading

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,34 @@
 <script setup lang="ts">
-import { watch } from 'vue'
+import { onMounted, watch } from 'vue'
 import { useFetch } from './helpers'
 import { store } from './store'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
+const router = useRouter()
+
+const fetchProfile = async () => {
+  if (store.user.username || route.path === '/login' || route.path === '/register') {
+    return
+  }
+
+  const { data } = await useFetch(`${import.meta.env.VITE_BASE_URL}/auth/profile`)
+    .get()
+    .json()
+
+  store.user = data.value
+}
+
+onMounted(async () => {
+  await router.isReady()
+  fetchProfile()
+})
 
 watch(
   () => route.path,
-  async (newPath) => {
-    if (!store.user.username && newPath !== '/login' && newPath !== '/register') {
-      const { data } = await useFetch(`${import.meta.env.VITE_BASE_URL}/auth/profile`)
-        .get()
-        .json()
-
-      store.user = data.value
-    }
+  () => {
+    fetchProfile()
   },
-  { immediate: true },
 )
 </script>
 
